d0a206848dcbe8ac6fa7f5ba5796b612726babca
Jacksen Narváez <janarvaez@unal.edu.co>
2022-02-06 18:04:06 -0500

    Quadratic regression for strong scaling results

--- fit.cpp
+++ fit.cpp
@@ -10,7 +10,7 @@
 //g++ -std=c++17  fit.cpp -lgsl -lgslcblas
 
 void read_data(const std::string &File_address, std::vector<double>& vector, int column);
-void Map(const std::vector<double>& x, std::vector<double>& y, int &n);
+void Map(const std::vector<double>& x, std::vector<double>& y, const int &n);
 
 int main(int argc, char **argv){
   std::vector<double> x;
@@ -27,37 +27,54 @@ int main(int argc, char **argv){
   double c0, c1, cov00, cov01, cov11, sumsq;
   gsl_fit_linear (x.data(),1,y.data(),1,n, &c0, &c1, &cov00, &cov01, &cov11, &sumsq);
   
-  double R1=gsl_stats_correlation (x.data(),1,y.data(),1,n);
+  double R1 = gsl_stats_correlation (x.data(),1,y.data(),1,n);
   
   std::cout<<c0 << "x + " << c1 <<"x^2"<<"\t"<<R1*R1<<"\n";
   return 0;
 }
 
 void read_data(const std::string &File_address, std::vector<double>& vector, int column){
-    std::ifstream File;
-    File.open (File_address, std::ifstream::in);    // Open file
-    std::string line;
-    std::istringstream data;
+  /*---------------------------------------------------------------------------
+  read_data:
+  Read the column <column> from <File_address>.
+  -----------------------------------------------------------------------------
+  Arguments:
+    File_address:   File address from which the data is read.
+    Vector      :   Vector to store the data.
+    column      :   Number of column for reading.
+  ---------------------------------------------------------------------------*/
+  std::ifstream File;
+  File.open (File_address, std::ifstream::in);    // Open file
+  std::string line;
+  std::istringstream data;
 	while (!File.eof()){
 	std::getline(File,line);
-    // Omit empty lines and comments
+  // Omit empty lines and comments
 	if (line.length() == 0 || line[0] == '#'){
 		continue;
-    }else{
-        std::istringstream iss(line);   // Separate line in columns
-        std::string data;        
-        for (int ii=0; ii < column; ii++){
-            iss>>data;
-        }
-        vector.push_back(atof(data.c_str()));
-        }
+  }else{
+    std::istringstream iss(line);   // Separate line in columns
+    std::string data;        
+    for (int ii=0; ii < column; ii++){
+      iss>>data;
     }
-    File.close();
+    vector.push_back(atof(data.c_str()));
+  }
+  }
+  File.close();
 }
 
-void Map(const std::vector<double>& x, std::vector<double>& y, int &n){
-  int i=0;
-  for (i=0; i<n; i++){
-    y[i]/=x[i];
+void Map(const std::vector<double>& x, std::vector<double>& y, const int &n){
+  /*---------------------------------------------------------------------------
+  Map:
+  Map y(x) to z(x)=y(x)/x .
+  -----------------------------------------------------------------------------
+  Arguments:
+    x:   Domain.
+    y:   y(x).
+    n:   Size
+  ---------------------------------------------------------------------------*/
+  for (int ii=0; ii<n; ii++){
+    y[ii]/=x[ii];
   }
 }
